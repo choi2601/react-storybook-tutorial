09004bba43359880b854eec88981771f2a53a203
choi2601 <byeonghyeonchoe55@gmail.com>
2022-03-05 18:34:38 +0900

    Add: InboxScreen Presentation Component 추가

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import { action } from '@storybook/addon-actions';
+// import { PureInboxScreen } from './InboxScreen';
+// import * as TaskListStories from './TaskList.stories';
+
+// const store = {
+//     getState: () => {
+//         return {
+//             tasks: TaskListStories.Default.args.tasks
+//         }
+//     },
+//     subscribe: () => 0,
+//     dispatch: action('dispatch'),
+// }
+
+// export default 
